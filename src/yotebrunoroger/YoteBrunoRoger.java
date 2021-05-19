29da24fae00c79aa43d1629c5b4597e0583d6446
roger <rogerionsilva20000@gmail.com>
2021-05-19 21:28:00 +0100

    Teste

--- src/yotebrunoroger/YoteBrunoRoger.java
+++ src/yotebrunoroger/YoteBrunoRoger.java
@@ -32,6 +32,7 @@ public class YoteBrunoRoger extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+        System.out.println("Hello");
     }
     
 }
